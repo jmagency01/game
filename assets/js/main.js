189571114441cb3f45924bc05e78fa6469046193
jmagency01 <jmagency@mail.ru>
2018-11-17 10:57:46 +0300

    Итог работы

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var game;
+game = new Phaser.Game(600,500,Phaser.AUTO, '');
+game.state.add('Menu', Menu);
+game.state.add('Game', Game);
+game.state.add('Game_Over', Game_Over);
+game.state.start('Menu');
+
